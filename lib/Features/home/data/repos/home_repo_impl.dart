ff8d5497d04659da3d4fa4928cb71c420dfc7e1b
Salma Ashraf <salmabenashrafbenfathy@gmail.com>
2025-12-22 19:03:17 +0300

    Create Api Service File

---
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,19 @@
+import 'package:dartz/dartz.dart';
+import 'package:my_book_shelf/Features/home/data/models/book_model/book_model.dart';
+import 'package:my_book_shelf/Features/home/data/repos/home_repo.dart';
+import 'package:my_book_shelf/core/errors/failures.dart';
+
+class HomeRepoImpl implements HomeRepo{
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
+    // TODO: implement fetchBestSellerBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+
+}
